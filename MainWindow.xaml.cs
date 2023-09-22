a2c5039ae3cfd5d87406735dd16b59e5e12297d2
zcBruAll <allan.brunner@zeacold.com>
2023-09-22 10:54:31 +0200

    Commented and changed code elements names

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,31 +20,41 @@ namespace ZC.TimeCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        int hStart, mStart;
-        int hEnd, mEnd;
-        int hStartPause, mStartPause;
-        int hEndPause, mEndPause;
-        int hSupp, mSupp;
+        int hStart, mStart;                     // Start time in hours and minutes separated
+        int hEnd, mEnd;                         // End time in hours and minutes separated
+        int hStartBreak, mStartBreak;           // Start break time in hours and minutes separated
+        int hEndBreak, mEndBreak;               // End break time in hours and minutes separated
+        int hSupp, mSupp;                       // Supp hours in hours and minutes separated
 
         List<TextBox> textBoxes = new List<TextBox>();
 
+        /// <summary>
+        /// On double-click, toggle mode between supp end time and default end time calculation
+        /// </summary>
+        /// <param name="sender">Not used</param>
+        /// <param name="e">Not used</param>
         private void txtEnd_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+            // If end time was already calculated
             if (hEnd > 0 && mEnd > 0)
             {
+                // Toggle read-only mode on textBoxes
                 txtHEnd.IsReadOnly = !txtHEnd.IsReadOnly;
                 txtMEnd.IsReadOnly = !txtMEnd.IsReadOnly;
 
+                // If end fields are read-only
                 if (txtHEnd.IsReadOnly)
                 {
+                    // Reset end time values to the calculated ones and h.supp to none
                     txtHEnd.Text = hEnd.ToString("D2");
                     txtMEnd.Text = mEnd.ToString("D2");
 
-                    txtHTimeSupp.Text = "";
-                    txtMTimeSupp.Text = "";
+                    txtHSupp.Text = "";
+                    txtMSupp.Text = "";
 
-                    txtHTimeSupp.Focus();
+                    txtHSupp.Focus();
                 }
+                // Else set focus on the end hour field
                 else
                 {
                     txtHEnd.Focus();
@@ -53,37 +63,55 @@ namespace ZC.TimeCalculator
             }
         }
 
+        /// <summary>
+        /// On text changes, calculates h.supp
+        /// </summary>
+        /// <param name="sender">The textbox that triggered the event</param>
+        /// <param name="e">Not used</param>
         private void txtEnd_TextChanged(object sender, TextChangedEventArgs e)
         {
             TextBox textBox = sender as TextBox;
+
+            // If textbox is read-only
             if (textBox.IsReadOnly) return;
 
+            // If the source textbox is the hour field
             if (textBox.Name.Contains("H"))
             {
+                // If the textbox's text has a length of 2
                 if (textBox.Text.Length == 2)
                 {
                     txtMEnd.Focus();
                     if (txtMEnd.Text.Length > 0) txtMEnd.SelectAll();
                 }
+                // If length is more than 2
                 else if (textBox.Text.Length > 2) textBox.Text = textBox.Text.Substring(0, 2);
             }
+            // If end time was calculated and both end fields content are set
             if (hEnd > 0 && mEnd > 0 && txtHEnd.Text.Length == 2 && txtMEnd.Text.Length == 2)
             {
+                // Delta of hours and minutes
                 hSupp = int.Parse(txtHEnd.Text) - hEnd;
                 mSupp = int.Parse(txtMEnd.Text) - mEnd;
+
+                // If delta of minutes is negative
                 if (mSupp < 0)
                 {
+                    // Transform negative supp minutes into supp hours
                     hSupp--;
                     mSupp = 60 + mSupp;
                 }
-                if (mSupp > 60)
+                // If delta of minutes is over 60
+                else if (mSupp > 60)
                 {
+                    // Transform over 60 supp minutes into supp hours
                     hSupp++;
                     mSupp -= 60;
                 }
 
-                txtHTimeSupp.Text = hSupp.ToString("D2");
-                txtMTimeSupp.Text = mSupp.ToString("D2");
+                // Display the results in 2 digits
+                txtHSupp.Text = hSupp.ToString("D2");
+                txtMSupp.Text = mSupp.ToString("D2");
             }
         }
 
@@ -91,7 +119,10 @@ namespace ZC.TimeCalculator
         {
             InitializeComponent();
 
+            // Start the app with a focus on the start field
             txtHStart.Focus();
+            
+            // Get all the references of textboxes of the app into a list
             foreach (var item in mainGrid.Children)
             {
                 if (item.GetType() == txtHStart.GetType())
@@ -101,19 +132,30 @@ namespace ZC.TimeCalculator
             }
         }
 
+        /// <summary>
+        /// On text changes, calculates end time
+        /// </summary>
+        /// <param name="sender">The textbox that triggered the event</param>
+        /// <param name="e">Not used</param>
         private void txt_TextChanged(object sender, TextChangedEventArgs e)
         {
             TextBox textBox = sender as TextBox;
+
+            // If textbox's text length equals 2
             if (textBox.Text.Length == 2)
             {
+                // If textbox's text is higher or equal than 0
                 if (int.Parse(textBox.Text) >= 0)
                 {
+                    // If textbox's name start with H and its text is lower than 24 or its name starts with M and its text is lower than 60
                     if (int.Parse(textBox.Text) < 24 && textBox.Name.Contains("H") || int.Parse(textBox.Text) < 60 && textBox.Name.Contains("M"))
                     {
+                        // Get the event source textBox reference in the list 
                         int idx = 0;
                         foreach (TextBox item in textBoxes)
                         {
                             ++idx;
+                            // If item is the event source textbox
                             if (item == textBox)
                             {
                                 textBoxes[idx].Focus();
@@ -124,120 +166,166 @@ namespace ZC.TimeCalculator
                             }
                         }
                     }
+                    // Else reset its text
                     else
                     {
                         textBox.Text = "";
                     }
                 }
+                // Else reset its text
                 else
                 {
                     textBox.Text = "";
                 }
             }
+            // Else if textbox's text length is over 2
             else if (textBox.Text.Length > 2)
             {
                 textBox.Text = textBox.Text.Substring(0, 2);
             }
 
+            // If all the field required to calculate end time are sets
             if (txtHStart.Text.Length == 2 && txtHStartPause.Text.Length == 2 && txtHEndPause.Text.Length == 2 &&
                 txtMStart.Text.Length == 2 && txtMStartPause.Text.Length == 2 && txtMEndPause.Text.Length == 2)
             {
+                // Retrieve all fields value into variables
                 hStart = int.Parse(txtHStart.Text);
                 mStart = int.Parse(txtMStart.Text);
-                hStartPause = int.Parse(txtHStartPause.Text);
-                mStartPause = int.Parse(txtMStartPause.Text);
-                hEndPause = int.Parse(txtHEndPause.Text);
-                mEndPause = int.Parse(txtMEndPause.Text);
+                hStartBreak = int.Parse(txtHStartPause.Text);
+                mStartBreak = int.Parse(txtMStartPause.Text);
+                hEndBreak = int.Parse(txtHEndPause.Text);
+                mEndBreak = int.Parse(txtMEndPause.Text);
 
-                calculateEndPause();
+                // Start calculating end time
+                calculateEnd();
             }
         }
 
+        /// <summary>
+        /// On text changes, calculates supp end time
+        /// </summary>
+        /// <param name="sender">The textbox that triggered the event</param>
+        /// <param name="e">Not used</param>
         private void txt_TextSuppChanged(object sender, TextChangedEventArgs e)
         {
+            // If end fields are read-only
             if (txtHEnd.IsReadOnly)
             {
-                if ((txtHTimeSupp.Text.Length == 2 && !txtHTimeSupp.Text.StartsWith("-")) || txtHTimeSupp.Text.Length == 3 && txtHTimeSupp.Text.StartsWith("-"))
+                // If supp hours length is 2 and not negative or length is 3 and negative
+                if ((txtHSupp.Text.Length == 2 && !txtHSupp.Text.StartsWith("-")) || txtHSupp.Text.Length == 3 && txtHSupp.Text.StartsWith("-"))
                 {
-                    txtMTimeSupp.Focus();
+                    txtMSupp.Focus();
                 }
+                // If all the values required to calculate supp end time are sets
                 if (txtHEnd.Text.Length == 2 &&
-                    ((txtHTimeSupp.Text.Length == 2 && !txtHTimeSupp.Text.StartsWith("-")) ||
-                    (txtHTimeSupp.Text.Length == 3 && txtHTimeSupp.Text.StartsWith("-"))) &&
-                    txtMTimeSupp.Text.Length == 2)
+                    ((txtHSupp.Text.Length == 2 && !txtHSupp.Text.StartsWith("-")) ||
+                    (txtHSupp.Text.Length == 3 && txtHSupp.Text.StartsWith("-"))) &&
+                    txtMSupp.Text.Length == 2)
                 {
-                    calculateHSupp();
+                    // Start calculating supp end time
+                    calculateHEndSupp();
                 }
             }
         }
 
-        private void calculateEndPause()
+        /// <summary>
+        /// Calculate the default end time
+        /// </summary>
+        private void calculateEnd()
         {
-            int hRemaining, mRemaining;
-            int hMatin, mMatin;
+            int hRemaining, mRemaining;             // Remaining time in hours and minutes separated
+            int hMorning, mMorning;                 // hours done in the morning
 
-            hMatin = hStartPause - hStart;
-            if (mStartPause < mStart)
+            // Calculate the hours done in the morning
+            hMorning = hStartBreak - hStart;
+            // If start time minutes are higher than start break time minutes
+            if (mStartBreak < mStart)
             {
-                --hMatin;
-                mMatin = 60 - mStart + mStartPause;
+                // Remove an hour and adapt minutes in consequence
+                --hMorning;
+                mMorning = 60 - mStart + mStartBreak;
             }
+            // Else basic calculation
             else
             {
-                mMatin = mStartPause - mStart;
+                mMorning = mStartBreak - mStart;
             }
 
-            hRemaining = 8 - hMatin;
-            if (mMatin < 24)
+            // Hours remaining to finish the day
+            hRemaining = 8 - hMorning;
+            // If minutes dones are lower than required
+            if (mMorning < 24)
             {
+                // Remove an hour and adapt minutes in consequence
                 --hRemaining;
-                mRemaining = 60 - mMatin + 24;
+                mRemaining = 60 - mMorning + 24;
             }
+            // Else basic calculation
             else
             {
-                mRemaining = 24 - mMatin;
+                mRemaining = 24 - mMorning;
             }
 
-            hEnd = hEndPause + hRemaining;
-            if (mRemaining + mEndPause > 60)
+            // Calculate the end hour
+            hEnd = hEndBreak + hRemaining;
+            // If sum of minutes remaining and minutes of the end break time are higher than 60
+            if (mRemaining + mEndBreak > 60)
             {
+                // Add an hor and adapt minutes in consequence
                 ++hEnd;
-                mEnd = mRemaining + mEndPause - 60;
+                mEnd = mRemaining + mEndBreak - 60;
             }
+            // Else basic calculation
             else
             {
-                mEnd = mRemaining + mEndPause;
+                mEnd = mRemaining + mEndBreak;
             }
 
+            // Display the result in 2 digits
             txtHEnd.Text = hEnd.ToString("D2");
             txtMEnd.Text = mEnd.ToString("D2");
         }
 
-        private void calculateHSupp()
+        /// <summary>
+        /// Calculate the end time with supp hours
+        /// </summary>
+        private void calculateHEndSupp()
         {
-            int hEndv2 = int.Parse(txtHEnd.Text) - int.Parse(txtHTimeSupp.Text);
+            // Calculate supp end time hours
+            hSupp = int.Parse(txtHEnd.Text) - int.Parse(txtHSupp.Text);
 
-            if (hEndv2 >= 1)
+            // If supp end time hour is higher or equal to 1
+            if (hSupp >= 1)
             {
-                int minTimeSupp = int.Parse(txtHTimeSupp.Text) < 0 ? int.Parse(txtMTimeSupp.Text) * -1 : int.Parse(txtMTimeSupp.Text);
-                int mEndv2 = int.Parse(txtMEnd.Text) - minTimeSupp;
-                if (mEndv2 < 0)
+                // If supp hours are negatives, sets negative minutes else sets positive minutes
+                mSupp = int.Parse(txtHSupp.Text) < 0 ? int.Parse(txtMSupp.Text) * -1 : int.Parse(txtMSupp.Text);
+                // Calculates supp end time minutes
+                mSupp = int.Parse(txtMEnd.Text) - mSupp;
+
+                // If supp end time minutes are lower than 0
+                if (mSupp < 0)
                 {
-                    --hEndv2;
-                    mEndv2 = 60 + mEndv2;
+                    // Remove an hour and adapt minutes in consequence
+                    --hSupp;
+                    mSupp = 60 + mSupp;
                 }
-                else if (mEndv2 >= 60)
+                // Else if supp end time minutes are higher than 60
+                else if (mSupp >= 60)
                 {
-                    ++hEndv2;
-                    mEndv2 -= 60;
+                    // Add an hour and adapt minutes in consequences
+                    ++hSupp;
+                    mSupp -= 60;
                 }
-                txtHEndv2.Text = hEndv2.ToString("D2");
-                txtMEndv2.Text = mEndv2.ToString("D2");
+
+                // Display results in 2 digits
+                txtHEndSupp.Text = hSupp.ToString("D2");
+                txtMEndSupp.Text = mSupp.ToString("D2");
             }
+            // Else display start time
             else
             {
-                txtHEndv2.Text = txtHStart.Text;
-                txtMEndv2.Text = txtMStart.Text;
+                txtHEndSupp.Text = txtHStart.Text;
+                txtMEndSupp.Text = txtMStart.Text;
             }
         }
     }
