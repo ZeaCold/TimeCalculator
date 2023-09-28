52d19ff775b2a360142a17c00b6ecd2009151faa
zcBruAll <allan.brunner@zeacold.com>
2023-09-28 09:05:54 +0200

    Added comments and small add-on on left and right key

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,103 +31,190 @@ namespace ZC.TimeCalculator
 
         List<TextBox> textBoxes = new List<TextBox>();      // List of all textboxes in the app
 
+        /// <summary>
+        /// On preview key down, only allow specified keys
+        /// and write specfic character
+        /// </summary>
+        /// <param name="sender">The textbox that triggered the event</param>
+        /// <param name="e">Args of the key event</param>
         private void Field_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             TextBox textBox = sender as TextBox;
 
+            // marque l'évènement comme géré
             e.Handled = true;
 
+            // Defines the minimum limit of the caret index
             int limit = textBox.Text.StartsWith("-") ? 1 : 0;
 
+            // If the caret index is under the limit, assign the limit value to the caret index
             if (textBox.CaretIndex < limit) textBox.CaretIndex = limit;
 
             int caretPosition = textBox.CaretIndex;
 
             string keyName = e.Key.ToString();
             string keyValue = "";
+            // If the key is a digit
             if (e.Key.IsDigit())
             {
+                // If the caret position is at the end of the text, return
                 if (caretPosition == textBox.Text.Length) return;
+                // Get the digit value at the end of the key name
                 keyValue = keyName.Substring(keyName.Length - 1, 1);
+                // If the caret position is on a colon, get to next position
                 if (textBox.Text[caretPosition] == ':') caretPosition++;
             }
+            // If the caret index is higher than the limit and the key is the backspace key
             else if (caretPosition > limit && e.Key == Key.Back)
             {
                 keyValue = "_";
+                // Decrement the caret position to be before the character to change
                 caretPosition--;
+                // If the caret position is on a colon, get to the previous position
                 if (textBox.Text[caretPosition] == ':') caretPosition--;
             }
+            // If the key is the delete key
             else if (e.Key == Key.Delete)
             {
                 keyValue = "_";
+                // If the caret position is on a colon, get to the next position
                 if (textBox.Text[caretPosition] == ':') caretPosition++;
             }
-            else if (e.Key == Key.Left && textBox.CaretIndex > limit)
-                textBox.CaretIndex = caretPosition - 1;
+            // If the key is the left key
+            else if (e.Key == Key.Left)
+            {
+                // If the caret index is higher than the limit
+                if (textBox.CaretIndex > limit)
+                    // Move the caret index by 1 to the left
+                    textBox.CaretIndex = caretPosition - 1;
+                // Else get the focus on the previous field
+                else FocusPreviousField(textBox);
+            }
+            // If the key is the right key
             else if (e.Key == Key.Right)
-                textBox.CaretIndex = caretPosition + 1;
-            else if (e.Key == Key.Tab) 
+            {
+                // If the caret index is not at the end of the textbox
+                if (textBox.CaretIndex < textBox.Text.Length)
+                    // Move the caret index by 1 to the right
+                    textBox.CaretIndex = caretPosition + 1;
+                // Else get the focus on the next field
+                else FocusNextField(textBox);
+            }
+            // If the key is the tab key
+            else if (e.Key == Key.Tab)
+                // Get the focus on the next field
                 FocusNextField(textBox);
+            // If the textbox is the txtTimeSupp TextBox
             else if (textBox.Equals(txtTimeSupp))
             {
+                // If the key is one of the minus key and the textbox's text doesn't start with a minus
                 if (e.Key == Key.Subtract || e.Key == Key.OemMinus && !textBox.Text.StartsWith("-"))
                 {
+                    // Add a minus to the textbox's text
                     textBox.Text = "-" + textBox.Text;
+                    // Set the caret index next the minus character
                     textBox.CaretIndex = 1;
                 }
+                // If the key is the add key and the textbox's text starts with a minus, removes it
                 else if (e.Key == Key.Add && textBox.Text.StartsWith("-")) textBox.Text = textBox.Text.Substring(1);
             }
 
+            // If the key value has been set
             if (keyValue != "")
             {
+                // Insert the key value at the current caret position in a new string
                 string time = textBox.Text.Substring(0, caretPosition) + keyValue +
                     textBox.Text.Substring(caretPosition + 1, textBox.Text.Length - (caretPosition + 1));
+                // If the new time matches the default time regex
                 if (timeFormat.IsMatch(time))
                 {
+                    // Assign the new time to the textBox's text
                     textBox.Text = time;
+                    // If the key is the backspace key
                     if (e.Key == Key.Back)
                     {
+                        // If the caret position is higher than the limit and the char at this position is a colon, move backward the caret index by 1
                         if (caretPosition > limit && textBox.Text[caretPosition - 1] == ':') textBox.CaretIndex = caretPosition - 1;
+                        // Else assign the current caret position to the caret index
                         else textBox.CaretIndex = caretPosition;
                     }
+                    // If the key is the delete key, assign the current caret position to the caret index
                     else if (e.Key == Key.Delete) textBox.CaretIndex = caretPosition;
+                    // If the next caret position is not the last position
                     else if (caretPosition + 1 < textBox.Text.Length)
                     {
+                        // Move the caret index forward by 1
                         textBox.CaretIndex = caretPosition + 1;
+                        // If the character at the caret position is a colon, move forward the caret index by 1
                         if (textBox.Text[textBox.CaretIndex] == ':') textBox.CaretIndex = textBox.CaretIndex + 1;
                     }
+                    // Else get the focus on the next field
                     else FocusNextField(textBox);
                 }
             }
         }
 
+        /// <summary>
+        /// Get the focus on the next field of the window
+        /// </summary>
+        /// <param name="currentTextBox">The current textBox</param>
         private void FocusNextField(TextBox currentTextBox)
         {
             // Get the event source textBox reference in the list 
             int idx = 0;
             foreach (TextBox item in textBoxes)
             {
-                ++idx;
-                // If item is the event source textbox
-                if (item == currentTextBox)
+                // If item is the event source textbox and the textbox is not the last one
+                if (item == currentTextBox && idx < textBoxes.Count - 1)
                 {
-                    textBoxes[idx].Focus();
+                    textBoxes[idx + 1].Focus();
                 }
+                ++idx;
             }
         }
 
+
+        /// <summary>
+        /// Get the focus on the previous field of the window
+        /// </summary>
+        /// <param name="currentTextBox">The current textBox</param>
+        private void FocusPreviousField(TextBox currentTextBox)
+        {
+            // Get the event source textBox reference in the list 
+            int idx = 0;
+            foreach (TextBox item in textBoxes)
+            {
+                // If item is the event source textbox and the index is higher than 0
+                if (item == currentTextBox && idx > 0)
+                {
+                    textBoxes[idx - 1].Focus();
+                }
+                ++idx;
+            }
+        }
+
+        /// <summary>
+        /// On text changes, calculate the new end time
+        /// </summary>
+        /// <param name="sender">Not used</param>
+        /// <param name="e">Not used</param>
         private void FieldTimeRequired_TextChanged(object sender, TextChangedEventArgs e)
         {
+            // If the text matches the finished time format
             if (timeFinishedFormat.IsMatch(txtTimeRequired.Text))
             {
+                // Create a table of the hours and minutes splitted
                 string[] fieldRequired = txtTimeRequired.Text.Split(':');
 
+                // Assign to the time required int table the new time required string values
                 timeRequired[0] = int.Parse(fieldRequired[0]);
                 timeRequired[1] = int.Parse(fieldRequired[1]);
 
+                // If the components are initialized and every textbox's text matches the finished time format
                 if (componentInitialized && timeFinishedFormat.IsMatch(txtTimeStart.Text) && 
                     timeFinishedFormat.IsMatch(txtTimeStartPause.Text) && 
                     timeFinishedFormat.IsMatch(txtTimeEndPause.Text))
+                    // Calculate the new end time
                     CalculateEnd();
             }
         }
@@ -190,16 +277,17 @@ namespace ZC.TimeCalculator
                     // If delta of minutes is negative
                     if (timeSupp[1] < 0)
                     {
+                        // If delta of hours is different from 0
                         if (timeSupp[0] != 0)
                         {
                             // Transform negative supp hour minutes into supp hours
                             timeSupp[0]--;
                             timeSupp[1] += M_In_H;
                         }
-                        else
-                        {
-                            isNegativeBut0Hour = true;
-                        }
+                        // Else assign true to the 0 hours negative flag
+                        else isNegativeBut0Hour = true;
+
+                        // Multiply supp minutes by -1 to make the positive
                         timeSupp[1] *= -1;
                     }
                     // If delta of minutes is over an hour
@@ -230,6 +318,7 @@ namespace ZC.TimeCalculator
             
             // Get all the references of textboxes of the app into a list
             foreach (var item in mainGrid.Children)
+                // If the item type is of the TextBox type
                 if (item.GetType() == txtTimeStart.GetType())
                     textBoxes.Add(item as TextBox);
         }
@@ -241,6 +330,7 @@ namespace ZC.TimeCalculator
         /// <param name="e">Not used</param>
         private void FieldsWorkTime_TextChanged(object sender, TextChangedEventArgs e)
         {
+            // If the component are initialized
             if (componentInitialized)
             {
                 TextBox textBox = sender as TextBox;
@@ -274,6 +364,7 @@ namespace ZC.TimeCalculator
         /// <param name="e">Not used</param>
         private void FieldsSupp_TextChanged(object sender, TextChangedEventArgs e)
         {
+            // If the component are initialized
             if (componentInitialized)
             {
                 // If end fields are read-only
@@ -292,7 +383,7 @@ namespace ZC.TimeCalculator
         /// </summary>
         private void CalculateEnd()
         {
-            int[] timeRemaining = new int[2];             // Remaining time in hours and minutes separated
+            int[] timeRemaining = new int[2];               // Remaining time in hours and minutes separated
             int[] timeMorning = new int[2];                 // Time done in the morning in hours and minutes separated
 
             // Delta of hours and minutes done in the morning
